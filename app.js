832985bb002977f3281d4e886c9b0235198745fe
lolligolden <lolligolden@github.com>
2024-10-11 17:12:34 -0500

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {});
+
+connect.then(() => console.log('Connected correcly to server'),
+  err => console.log(err)
+);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
